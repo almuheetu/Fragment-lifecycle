9a82b5818ff933e2cb410e7b44cf17fe3b75c852
Almuheetu Shihab <almuheetu@gmail.com>
2024-06-25 18:18:05 +0600

    passing data activity to fragment

--- app/src/main/java/com/example/fragmentlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/fragmentlifecycle/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.fragmentlifecycle
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import com.example.fragmentlifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        if(savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<FirstFragment>(R.id.nav_host_fragment_first)
+            }
+        }
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -23,9 +33,42 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-//        binding.next_btn.setOnClickListener {
-//            val intent = Intent(this, FirstFragment::class.java)
-//            startActivity(intent)
-//        }
+
+
+
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG", "onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("TAG", "onSaveInstanceState")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG", "onDestroy")
+    }
+
+
 }
